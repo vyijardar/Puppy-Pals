26736455d254bbdbf9f39662ebe7f52e0deb2e16
feature-A <vyijardar@YuVi>
2024-10-31 14:32:49 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-
+import { useState } from 'react';
+import { puppyList } from './data.js'
 import './App.css'
 
+
 function App() {
- 
+  console.log(puppyList);
+  const [puppies,setPuppies] = useState(puppyList);
   return (
     <>
- 
       <div className="App">
-       
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
       </div>
-    
+
     </>
   )
 }
