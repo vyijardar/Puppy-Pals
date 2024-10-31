3c5a6d42414687e4680d2a7e21127b5b6aed9678
feature-A <vyijardar@YuVi>
2024-10-31 16:06:19 -0500

    Updated react code

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,33 @@ import { useState } from 'react';
 import { puppyList } from './data.js'
 import './App.css'
 
-
 function App() {
-  console.log(puppyList);
-  const [puppies,setPuppies] = useState(puppyList);
+
+
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
+  console.log('puppies', puppyList);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <>
       <div className="App">
-      {
-        puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
-        })
-      }
+        {
+          puppies.map((puppy) => {
+            return <p onClick={() => {
+              console.log("puppy id: ", puppy.id);
+              setFeatPupId(puppy.id);
+            }} key={puppy.id}>{puppy.name}</p>
+          })
+        }
+        {featPupId && <div className='puppyInfo'>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>}
       </div>
 
     </>
